672d996e6bb3ad8b9eb2a54c7bc0f6bc517380b0
koyu <me@koyu.space>
2022-12-12 20:10:53 +0100

    Add emoji support

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ $(document).ready(() => {
         let acct = user.split("@")[0];
         let instance = user.split("@")[1];
         $.getJSON("https://"+instance+"/api/v1/accounts/lookup?acct="+user, (json) => {
-          $("#user-list").append(`<li><a href="https://${instance}/@${acct}" title="rolle" class="status__display-name" rel="noopener noreferrer"><div class="status__avatar"><div class="account__avatar" style="width: 46px; height: 46px;"><img src="${json.avatar}" alt="${acct}"></div></div><span class="display-name"><bdi><strong class="display-name__html">${json.display_name}</strong></bdi> <span class="display-name__account">${user}</span></span></a><a href="https://${instance}/@${acct}" class="button">Seuraa</a></li>`);
+          let display_name = json.display_name;
+          display_name = twemoji.parse(display_name, {className: "emojione"});
+          try {
+              if (json.emojis.length > 0) {
+                  json.emojis.forEach(dp_emoji => {
+                      display_name = display_name.replaceAll(`:${dp_emoji.shortcode}:`, `<img src="${dp_emoji.url}" alt="Emoji ${dp_emoji.shortcode}" class="emojione">`);
+                  });
+              }
+          } catch (e) {}
+          $("#user-list").append(`<li><a href="https://${instance}/@${acct}" title="rolle" class="status__display-name" rel="noopener noreferrer"><div class="status__avatar"><div class="account__avatar" style="width: 46px; height: 46px;"><img src="${json.avatar}" alt="${acct}"></div></div><span class="display-name"><bdi><strong class="display-name__html">${display_name}</strong></bdi> <span class="display-name__account">${user}</span></span></a><a href="https://${instance}/@${acct}" class="button">Seuraa</a></li>`);
         });
         counter++;
         $("#user-count").html(counter);
