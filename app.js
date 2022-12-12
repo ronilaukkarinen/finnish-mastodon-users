860c60376dc6e04a15cca45e60a2ff0eac9992b6
Roni Laukkarinen <roni@dude.fi>
2022-12-12 21:32:25 +0200

    Remove jQuery dependency, rewrite in Vanilla JS

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
-// User list
-$(document).ready(() => {
-  // Parse CSV and add users
-  $.get("following_accounts.csv", (result) => {
-    let csv = result;
-    let lines = csv.split("\n");
-    lines.shift();
-    $("#user-list").html("");
-    let counter = -1;
-    lines.forEach((line) => {
-      if (line !== "") {
-        let parts = line.split(",");
-        let user = parts[0];
-        let acct = user.split("@")[0];
-        let instance = user.split("@")[1];
-        $.getJSON("https://"+instance+"/api/v1/accounts/lookup?acct="+user, (json) => {
-          $("#user-list").append(`<li><a href="https://${instance}/@${acct}" title="rolle" class="status__display-name" rel="noopener noreferrer"><div class="status__avatar"><div class="account__avatar" style="width: 46px; height: 46px;"><img src="${json.avatar}" alt="${acct}"></div></div><span class="display-name"><bdi><strong class="display-name__html">${json.display_name}</strong></bdi> <span class="display-name__account">${user}</span></span></a><a href="https://${instance}/@${acct}" class="button">Seuraa</a></li>`);
-        });
-        counter++;
-        $("#user-count").html(counter);
-      }
-    });
+// Parse CSV and add users with Vanilla JS
+function parseCSV() {
+  // Get CSV file
+  var request = new XMLHttpRequest();
+  request.open("GET", "following_accounts.csv", false);
+  request.send(null);
+  var csv = request.responseText;
+
+  // Split CSV into lines
+  var lines = csv.split("\n");
+  lines.shift();
+
+  // Clear user list
+  document.getElementById("user-list").innerHTML = "";
+
+  // Add users
+  var counter = -1;
+  lines.forEach((line) => {
+    if (line !== "") {
+      var parts = line.split(",");
+      var user = parts[0];
+      var acct = user.split("@")[0];
+      var instance = user.split("@")[1];
+      var request = new XMLHttpRequest();
+      request.open("GET", "https://"+instance+"/api/v1/accounts/lookup?acct="+user, false);
+      request.send(null);
+      var json = JSON.parse(request.responseText);
+      document.getElementById("user-list").innerHTML += `<li><a href="https://${instance}/@${acct}" title="rolle" class="status__display-name" rel="noopener noreferrer"><div class="status__avatar"><div class="account__avatar" style="width: 46px; height: 46px;"><img src="${json.avatar}" alt="${acct}"></div></div><span class="display-name"><bdi><strong class="display-name__html">${json.display_name}</strong></bdi> <span class="display-name__account">${user}</span></span></a><a href="https://${instance}/@${acct}" class="button">Seuraa</a></li>`;
+      counter++;
+      document.getElementById("user-count").innerHTML = counter;
+    }
   });
+}
+
+// DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function(event) {
+  parseCSV();
 
   // Last updated
   moment.locale('fi');
