6d899cd72d8f48875ac557017a5dbdf2e77064fc
Roni Laukkarinen <roni@dude.fi>
2022-12-16 01:18:43 +0200

    Combine HTML

--- main.js
+++ main.js
@@ -407,7 +407,8 @@ addEventListener('DOMContentLoaded', (event) => {
 
 });
 
-const headerInfo = `
+document.querySelector('#app').innerHTML = `
+<section>
 <h1>
   <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><path fill="currentColor" d="M64 144c26.5 0 48-21.5 48-48s-21.5-48-48-48S16 69.5 16 96s21.5 48 48 48zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM64 464c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm48-208c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48z"/></svg>
   Suomalaiset Mastodon-käyttäjät
@@ -429,9 +430,7 @@ const headerInfo = `
     </div>
   </div>
 </div>
-`
 
-const instructions = `
 <!-- Accessible show instructions toggle -->
 <button class="button" id="show-instructions" aria-expanded="false" aria-controls="instructions"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM288 352c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z"/></svg>Haluatko listalle? Kuinka seurata? Katso ohjeet</button>
 
@@ -464,25 +463,13 @@ const instructions = `
     <li>Lataa <a href="https://fo.llow.social/roam">Roam Chrome-lisäosa</a> ja laita asetuksiin oman instanssisi osoite. Klikkaa alta <b>Siirry profiiliin</b>, sitten klikkaa selaimen lisäosapalkista lisäosan kuvaketta, josta pääset seuraamaan käyttäjää.</li>
   </ul>
 </div>
-`
 
-const footer = `
-<footer>
-<p>Käsin koodattu ja <a href="https://github.com/ronilaukkarinen/finnish-mastodon-users">avointa lähdekoodia</a>. <span class="updated">Päivitetty viimeksi <time id="updated"></time>.</span></p>
-</footer>
-`
-
-const usersContent = `
 <h2>Käyttäjät (<span class="user-count" id="user-count" aria-live="polite">...</span>)</h2>
 
 <ul class="user-list" id="user-list" aria-live="polite">Ladataan...</ul>
 </section>
-`
 
-document.querySelector('#app').innerHTML = `
-<section>
-${headerInfo}
-${instructions}
-${usersContent}
-${footer}
+<footer>
+<p>Käsin koodattu ja <a href="https://github.com/ronilaukkarinen/finnish-mastodon-users">avointa lähdekoodia</a>. <span class="updated">Päivitetty viimeksi <time id="updated"></time>.</span></p>
+</footer>
 `
