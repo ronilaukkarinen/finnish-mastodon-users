2ab993587e01b2158b1ac1b3dc1e3a101fbd2456
Roni Laukkarinen <roni@dude.fi>
2022-12-16 21:19:24 +0200

    Improve performance, hybrid between jquery and vanilla js

---
+++ app-vanilla.js
@@ -0,0 +1,187 @@
+// When DOM is ready
+addEventListener('DOMContentLoaded', () => {
+
+  // Update aria-busy for user-count
+  const userCount = document.getElementById('user-count');
+  userCount.setAttribute('aria-busy', 'true');
+
+  // Update aria-busy for user-list
+  const userList = document.getElementById('user-list');
+  userList.setAttribute('aria-busy', 'true');
+
+  // Parse CSV and add users
+  fetch("following_accounts.csv", { cache: "force-cache" })
+  .then(response => response.text())
+  .then(result => {
+    let csv = result;
+    let lines = csv.split("\n");
+
+    lines.shift();
+    document.getElementById("user-list").innerHTML = "";
+
+    // Cache lines to local storage for 15 minutes
+    localStorage.setItem("following_accounts", lines);
+    localStorage.setItem("following_accounts_timestamp", Date.now());
+
+    // Expire cache after 15 minutes
+    if (localStorage.getItem("following_accounts_timestamp") + 900000 < Date.now()) {
+      localStorage.removeItem("following_accounts");
+      localStorage.removeItem("following_accounts_timestamp");
+    }
+
+    // Serve from cache if available
+    if (localStorage.getItem("following_accounts")) {
+      lines = localStorage.getItem("following_accounts");
+
+      // Convert string to array
+      lines = lines.split(",");
+    }
+
+    let counter = 0;
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i];
+
+      if (line !== "") {
+        let parts = line.split(",");
+        let user = parts[0];
+        let acct = user.split("@")[0];
+        let instance = user.split("@")[1];
+
+        // Exceptions for vivaldi.net and testausserveri.fi
+        // Add instances to array
+        let instanceExceptions = [
+          "vivaldi.net",
+          "mastodon.ellipsis.fi",
+          "mastodon.testausserveri.fi",
+          "masto.henkkalaukka.fi",
+        ];
+
+        // Check if instance is in array
+        if (instanceExceptions.includes(instance)) {
+
+          // If instance is in array, change user to acct
+          user = acct;
+
+          // Exception for vivaldi.net
+          if ( instance === "vivaldi.net" ) {
+            instance = "social.vivaldi.net";
+          }
+        }
+
+        fetch("https://" + instance + "/api/v1/accounts/lookup?acct="+user, { cache: "force-cache" })
+        .then(response => response.json())
+        .then(json => {
+          let display_name = json.display_name;
+          let bio = json.note;
+              display_name = twemoji.parse(display_name, {className: "emojione"});
+              bio = twemoji.parse(bio, {className: "emojione"});
+
+            // Follow link/button
+            let followButton = `<a href="https://${instance}/@${user}" class="button button-action">Profiili</a>`;
+
+            try {
+              if (json.emojis.length > 0) {
+
+                json.emojis.forEach(dp_emoji => {
+                  display_name = display_name.replaceAll(`:${dp_emoji.shortcode}:`, `<img src="${dp_emoji.url}" alt="Emoji ${dp_emoji.shortcode}" class="emojione">`);
+                    bio = bio.replaceAll(`:${dp_emoji.shortcode}:`, `<img src="${dp_emoji.url}" alt="Emoji ${dp_emoji.shortcode}" class="emojione">`);
+                  });
+                }
+            } catch (e) {}
+
+            // User template
+            let userTemplate = `
+            <li class="account-card collapsed" id="user-${json.id}">\
+            <button id="button-${json.id}" class="button-collapse account-card__permalink" aria-label="Näytä käyttäjän ${acct} lisätiedot" aria-expanded="false" aria-controls="user-${json.id}">\
+              <span class="screen-reader-text">Näytä lisätiedot</span>\
+            </button>\
+              <div class="account-card__header" aria-hidden="true">\
+                <img src="${json.header}" alt="Käyttäjän ${acct} header-kuva">\
+              </div>\
+              <div class="account-card__title">\
+                <div class="account-card__title__avatar">\
+                  <div class="account__avatar" style="width: 56px; height: 56px;">\
+                    <img src="${json.avatar}" alt="Käyttäjän ${acct} profiilikuva">\
+                  </div>
+                </div>\
+                <span class="display-name">\
+                  <bdi>\
+                    <strong class="display-name__html">${display_name}</strong>\
+                  </bdi>\
+                  <span class="display-name__account">${user}</span>\
+                </span>\
+              </div>\
+            </a>\
+            <div class="account-card__bio">\
+              ${bio}\
+            </div>\
+            <div class="account-card__actions">\
+              <div class="account-card__counters">\
+                <div class="account-card__counters__item">\
+                  <span>${json.statuses_count}</span>\
+                  <small><span>Viestit</span></small>\
+                </div>\
+                <div class="account-card__counters__item">\
+                  <span>${json.following_count}</span>\
+                  <small><span>Seuraajat</span></small>\
+                </div>\
+                <div class="account-card__counters__item">\
+                  <span>${json.followers_count}</span>\
+                  <small><span>Seurataan</span></small>\
+                </div>\
+              </div>\
+              <div class="account-card__actions__button" id="actions__button-${json.id}">\
+                ${followButton}\
+              </div>\
+            </div>\
+            </li>`;
+
+            // Count users
+            counter++;
+
+            // Append userTemplate to user-list
+            document.getElementById("user-list").innerHTML += userTemplate;
+
+            // Update user-count
+            document.getElementById("user-count").innerHTML = counter;
+
+            // Update aria-busy for user-count
+            userCount.setAttribute('aria-busy', 'false');
+
+            // Update aria-busy for user-list
+            userList.setAttribute('aria-busy', 'false');
+
+          })
+          .catch(error => {
+            console.log(error);
+          }
+        );
+      }
+    }
+  });
+
+  // Expand/Collapse single account-card based on user id
+  function expandCollapseUser(id) {
+    let user = document.getElementById("user-"+id);
+    let button = document.getElementById("button-"+id);
+
+    if (user.classList.contains("collapsed")) {
+      user.classList.remove("collapsed");
+      button.getElementsByTagName("span")[0].innerHTML = "Piilota lisätiedot";
+      button.setAttribute("aria-expanded", "false");
+    } else {
+      user.classList.add("collapsed");
+      button.getElementsByTagName("span")[0].innerHTML = "Näytä lisätiedot";
+      button.setAttribute("aria-expanded", "true");
+    }
+  }
+
+  // Run expandCollapseUser() on click
+  document.addEventListener("click", function(e) {
+    if (e.target && e.target.id.includes("button-")) {
+      let id = e.target.id.split("button-")[1];
+      expandCollapseUser(id);
+    }
+  });
+
+});
--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-// When DOM is ready
-addEventListener('DOMContentLoaded', () => {
-
-  // Update aria-busy for user-count
-  const userCount = document.getElementById('user-count');
-  userCount.setAttribute('aria-busy', 'true');
-
-  // Update aria-busy for user-list
-  const userList = document.getElementById('user-list');
-  userList.setAttribute('aria-busy', 'true');
-
+// User list
+$(document).ready(() => {
   // Parse CSV and add users
-  fetch("following_accounts.csv")
-  .then(response => response.text())
-  .then(result => {
+  $.get("following_accounts.csv", (result) => {
     let csv = result;
     let lines = csv.split("\n");
-
     lines.shift();
-    document.getElementById("user-list").innerHTML = "";
 
-    let counter = 0;
+    // Update aria-busy for user-count
+    const userCount = document.getElementById('user-count');
+    userCount.setAttribute('aria-busy', 'true');
+
+    // Update aria-busy for user-list
+    const userList = document.getElementById('user-list');
+    userList.setAttribute('aria-busy', 'true');
+
+    $("#user-list").html("");
+
+    let counter = -1;
     lines.forEach((line) => {
       if (line !== "") {
         let parts = line.split(",");
@@ -48,9 +45,7 @@ addEventListener('DOMContentLoaded', () => {
           }
         }
 
-        fetch("https://"+instance+"/api/v1/accounts/lookup?acct="+user)
-        .then(response => response.json())
-        .then(json => {
+        $.getJSON("https://" + instance + "/api/v1/accounts/lookup?acct="+user, (json) => {
           let display_name = json.display_name;
           let bio = json.note;
               display_name = twemoji.parse(display_name, {className: "emojione"});
@@ -59,83 +54,82 @@ addEventListener('DOMContentLoaded', () => {
             // Follow link/button
             let followButton = `<a href="https://${instance}/@${user}" class="button button-action">Profiili</a>`;
 
-            try {
-              if (json.emojis.length > 0) {
-
+          try {
+            if (json.emojis.length > 0) {
                 json.emojis.forEach(dp_emoji => {
                   display_name = display_name.replaceAll(`:${dp_emoji.shortcode}:`, `<img src="${dp_emoji.url}" alt="Emoji ${dp_emoji.shortcode}" class="emojione">`);
-                    bio = bio.replaceAll(`:${dp_emoji.shortcode}:`, `<img src="${dp_emoji.url}" alt="Emoji ${dp_emoji.shortcode}" class="emojione">`);
-                  });
-                }
-            } catch (e) {}
+                  bio = bio.replaceAll(`:${dp_emoji.shortcode}:`, `<img src="${dp_emoji.url}" alt="Emoji ${dp_emoji.shortcode}" class="emojione">`);
+                });
+            }
+          } catch (e) {}
 
-            // User template
-            let userTemplate = `
-            <li class="account-card collapsed" id="user-${json.id}">\
-            <button id="button-${json.id}" class="button-collapse account-card__permalink" aria-label="Näytä käyttäjän ${acct} lisätiedot" aria-expanded="false" aria-controls="user-${json.id}">\
-              <span class="screen-reader-text">Näytä lisätiedot</span>\
-            </button>\
-              <div class="account-card__header" aria-hidden="true">\
-                <img src="${json.header}" alt="Käyttäjän ${acct} header-kuva">\
-              </div>\
-              <div class="account-card__title">\
-                <div class="account-card__title__avatar">\
-                  <div class="account__avatar" style="width: 56px; height: 56px;">\
-                    <img src="${json.avatar}" alt="Käyttäjän ${acct} profiilikuva">\
-                  </div>
-                </div>\
-                <span class="display-name">\
-                  <bdi>\
-                    <strong class="display-name__html">${display_name}</strong>\
-                  </bdi>\
-                  <span class="display-name__account">${user}</span>\
-                </span>\
-              </div>\
-            </a>\
-            <div class="account-card__bio">\
-              ${bio}\
+          // User template
+          let userTemplate = `
+          <li class="account-card collapsed" id="user-${json.id}">\
+          <button id="button-${json.id}" class="button-collapse account-card__permalink" aria-label="Näytä käyttäjän ${acct} lisätiedot" aria-expanded="false" aria-controls="user-${json.id}">\
+            <span class="screen-reader-text">Näytä lisätiedot</span>\
+          </button>\
+            <div class="account-card__header" aria-hidden="true">\
+              <img src="${json.header}" alt="Käyttäjän ${acct} header-kuva">\
             </div>\
-            <div class="account-card__actions">\
-              <div class="account-card__counters">\
-                <div class="account-card__counters__item">\
-                  <span>${json.statuses_count}</span>\
-                  <small><span>Viestit</span></small>\
-                </div>\
-                <div class="account-card__counters__item">\
-                  <span>${json.following_count}</span>\
-                  <small><span>Seuraajat</span></small>\
-                </div>\
-                <div class="account-card__counters__item">\
-                  <span>${json.followers_count}</span>\
-                  <small><span>Seurataan</span></small>\
-                </div>\
+            <div class="account-card__title">\
+              <div class="account-card__title__avatar">\
+                <div class="account__avatar" style="width: 56px; height: 56px;">\
+                  <img src="${json.avatar}" alt="Käyttäjän ${acct} profiilikuva">\
+                </div>
               </div>\
-              <div class="account-card__actions__button" id="actions__button-${json.id}">\
-                ${followButton}\
+              <span class="display-name">\
+                <bdi>\
+                  <strong class="display-name__html">${display_name}</strong>\
+                </bdi>\
+                <span class="display-name__account">${user}</span>\
+              </span>\
+            </div>\
+          </a>\
+          <div class="account-card__bio">\
+            ${bio}\
+          </div>\
+          <div class="account-card__actions">\
+            <div class="account-card__counters">\
+              <div class="account-card__counters__item">\
+                <span>${json.statuses_count}</span>\
+                <small><span>Viestit</span></small>\
+              </div>\
+              <div class="account-card__counters__item">\
+                <span>${json.following_count}</span>\
+                <small><span>Seuraajat</span></small>\
+              </div>\
+              <div class="account-card__counters__item">\
+                <span>${json.followers_count}</span>\
+                <small><span>Seurataan</span></small>\
               </div>\
             </div>\
-            </li>`;
+            <div class="account-card__actions__button" id="actions__button-${json.id}">\
+              ${followButton}\
+            </div>\
+          </div>\
+          </li>`;
 
-            // Count users
-            counter++;
+          // Count users
+          counter++;
 
-            // Append userTemplate to user-list
-            document.getElementById("user-list").innerHTML += userTemplate;
+          // Append userTemplate to user-list
+          $("#user-list").append(`${userTemplate}`);
 
-            // Update user-count
-            document.getElementById("user-count").innerHTML = counter;
+          // Update user-count
+          $("#user-count").html(counter);
+
+          }).fail((jqXHR, textStatus, errorThrown) => {
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + textStatus);
+            console.dir(jqXHR);
 
             // Update aria-busy for user-count
             userCount.setAttribute('aria-busy', 'false');
-
-            // Update aria-busy for user-list
-            userList.setAttribute('aria-busy', 'false');
-
-          })
-          .catch(error => {
-            console.log(error);
-          }
-        );
+          }).done(() => {
+            // Update aria-busy for user-count
+            userCount.setAttribute('aria-busy', 'false');
+          });
       }
     });
   });
@@ -163,5 +157,4 @@ addEventListener('DOMContentLoaded', () => {
       expandCollapseUser(id);
     }
   });
-
 });
